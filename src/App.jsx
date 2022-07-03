21ccd77a21559541bc8292341180ab0eacb1929c
Manjunath <rudrangimanjunath@gmail.com>
2022-07-03 19:06:19 +0530

    added navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import Navs from "./components/Navs";
+import Home from "./Pages/Home";
+import Starred from "./Pages/Starred";
+
 import { Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <Switch>
-      <Route exact={true} path="/">
-        This is home page
-      </Route>
-      <Route exact path="/starred">
-        This is starred page
-      </Route>
-      <Route>404 page not found</Route>
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact={true} path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>
+          <div>Page Not found</div>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
